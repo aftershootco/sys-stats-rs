c934c2acf36d785dc0445f681d61e872ecc9455f
Ishan Grover <ishangrover@MacBook-Pro.local>
2025-12-23 02:19:41 +0530

    refactor(memory, mac): Use sysinfo instead of the sys_stats

--- src/memory/macos.rs
+++ src/memory/macos.rs
@@ -1,39 +1,48 @@
-use crate::memory::{MemoryData, MemoryUsage};
+use sysinfo::{MemoryRefreshKind, RefreshKind};
 
-fn to_bytes(memory_data: &MemoryData) -> MemoryData {
-    MemoryData {
-        total: memory_data.total * 1024,
-        free: memory_data.free * 1024,
-        used: memory_data.used * 1024,
-    }
-}
+use crate::memory::{MemoryData, MemoryUsage};
 
 impl MemoryUsage {
     pub fn get_system_memory_info() -> Result<MemoryData, Box<dyn std::error::Error>> {
-        let mem_info = sys_info::mem_info()?;
-        Ok(to_bytes(&MemoryData::new_with_values(
-            mem_info.total,
-            mem_info.free,
-            mem_info.total - mem_info.free,
-        )))
+        let system = sysinfo::System::new_with_specifics(
+            RefreshKind::nothing().with_memory(MemoryRefreshKind::nothing().with_ram()),
+        );
+
+        Ok(MemoryData {
+            total: system.total_memory(),
+            free: system.free_memory(),
+            used: system.used_memory(),
+        })
     }
     pub fn total_system_memory() -> Result<u64, Box<dyn std::error::Error>> {
-        let mem_info = sys_info::mem_info()?;
-        Ok(mem_info.total * 1024)
+        let system = sysinfo::System::new_with_specifics(
+            RefreshKind::nothing().with_memory(MemoryRefreshKind::nothing().with_ram()),
+        );
+
+        Ok(system.total_memory())
     }
 
     pub fn current_system_memory_usage() -> Result<u64, Box<dyn std::error::Error>> {
-        let mem_info = sys_info::mem_info()?;
-        Ok((mem_info.total - mem_info.free) * 1024) // convert to bytes
+        let system = sysinfo::System::new_with_specifics(
+            RefreshKind::nothing().with_memory(MemoryRefreshKind::nothing().with_ram()),
+        );
+
+        Ok(system.used_memory())
     }
 
     pub fn current_system_memory_free() -> Result<u64, Box<dyn std::error::Error>> {
-        let mem_info = sys_info::mem_info()?;
-        Ok((mem_info.free) * 1024) // convert to bytes
+        let system = sysinfo::System::new_with_specifics(
+            RefreshKind::nothing().with_memory(MemoryRefreshKind::nothing().with_ram()),
+        );
+
+        Ok(system.free_memory())
     }
 
     pub fn current_system_memory_swap() -> Result<(u64, u64), Box<dyn std::error::Error>> {
-        let mem_info = sys_info::mem_info()?;
-        Ok((mem_info.swap_total * 1024, mem_info.swap_free * 1024)) // convert to bytes
+        let system = sysinfo::System::new_with_specifics(
+            RefreshKind::nothing().with_memory(MemoryRefreshKind::nothing().with_swap()),
+        );
+
+        Ok((system.used_swap(), system.total_swap()))
     }
 }
