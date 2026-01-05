a949481d8a437bb3155509cb41111925c21f0f67
Ishan Grover <ishangrover@MacBook-Pro-2.local>
2026-01-05 14:24:33 +0530

    feat(cpu): Add the Cpu instruction set feild

--- src/cpu/windows.rs
+++ src/cpu/windows.rs
@@ -1,4 +1,4 @@
-use crate::cpu::{CPUData, CPUUsage};
+use crate::cpu::{CPUData, CPUUsage, CpuFeatureSet};
 use anyhow::Result;
 use sysinfo::{CpuRefreshKind, RefreshKind, System};
 use winapi::um::sysinfoapi::GetSystemInfo;
@@ -13,16 +13,14 @@ impl CPUUsage {
             GetSystemInfo(&mut sys_info);
         }
 
-        let mut cpu_data = CPUData {
+        CPUData {
             name: Self::get_cpu_name(),
             vendor: Self::get_cpu_vendor(),
             architecture: Self::get_cpu_architecture(),
-            num_of_cores: 0,
-            average_cpu_usage: 0.0,
-        };
-        cpu_data.num_of_cores = Self::num_of_cores();
-        cpu_data.average_cpu_usage = Self::average_usage();
-        Ok(cpu_data)
+            num_of_cores: Self::num_of_cores(),
+            average_cpu_usage: Self::average_usage(),
+            instruction_sets: Self::get_instruction_sets(),
+        }
     }
 
     pub fn num_of_cores() -> u32 {
@@ -69,6 +67,21 @@ impl CPUUsage {
             12 => CPUArchitecture::Arm64,
             14 => CPUArchitecture::RiscV64,
             _ => CPUArchitecture::Unknown,
+        }
+    }
+
+    fn get_instruction_sets() -> Vec<CpuFeatureSet> {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            use strum::IntoEnumIterator;
+            CpuFeatureSet::iter()
+                .filter(|f| f.is_supported_x86())
+                .collect()
+        }
+
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        {
+            Vec::new()
         }
     }
 }
